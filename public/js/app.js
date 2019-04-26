375109e49ae6e6858ad90eba1c7bae6cf4ecd56b
TheOcelot <tenshiioannis@gmail.com>
2019-04-26 22:08:13 +0300

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('client side javascript file is loades')
-  
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
