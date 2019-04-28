37ca653eb006f546fe2be21a91e7fb7a1e08323c
TheOcelot <tenshiioannis@gmail.com>
2019-04-28 13:03:26 +0300

    created local dev dependencies

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,6 @@ const forecast = (latitude, longtitude, callback) => {
 	const url = 'https://api.darksky.net/forecast/2a8ab655fcdd0a3497b5cf45c9935c2f/'+ latitude +',' + longtitude + '?units=si'
 	
 	request({url, json: true},(error, {body}) => {
-		console.log(body)
 		if (error) {
 			callback('Unable to connect to weather service!', undefined)
 		} else if (body.error) {
